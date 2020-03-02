83f7b0afbdbafb76e6d786559ad6e72f68f636bf
Damely Tineo <damely.tineo07@gmail.com>
2020-03-02 04:47:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -92,12 +92,11 @@ class Dog
       LIMIT 1
     SQL
     
-    # DB[:conn].results_as_hash = true
+    DB[:conn].results_as_hash = true
     result = DB[:conn].execute(sql, name, breed)
-    puts result
     
     if !result.empty?
-      db_return = result[0]
+      #db_return = result[0]
       dog = Dog.new(id: db_return[0], name: db_return[1], breed: db_return[2])
     else 
       dog = Dog.create(name: name, breed: breed)
